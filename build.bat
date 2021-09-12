bcd9435cfeb13a6b85af6cfd41a5e0d950cce583
miile7 <miile7@gmx.de>
2021-09-12 11:00:22 +0200

    Fixing building and deploying to PyPi

--- build.bat
+++ build.bat
@@ -1,5 +1,35 @@
 @echo off
+setlocal
 
+echo "Installing python build package..."
+python -m pip install --upgrade build
+
+echo "Building app..."
 python -m build
+
+echo "Done."
+
+SET /P UPLOAD=Upload package (Y/[N])?
+IF /I "%UPLOAD%" NEQ "Y" GOTO END
+
+echo "Installing twine..."
+python -m pip install --upgrade twine
+
+SET /P UPLOAD=Test Upload (Y/[N])?
+IF /I "%UPLOAD%" NEQ "Y" GOTO PYPI_REPOSITORY
+
+SET UPLOAD_REPOSITORY=testpypi
+GOTO UPLOAD
+
+:PYPI_REPOSITORY
+SET UPLOAD_REPOSITORY=pypi
+GOTO UPLOAD
+
+:UPLOAD
+echo "Uploading"
+python -m twine upload --repository "%UPLOAD_REPOSITORY%" "./dist/*"
+
+:END
+endlocal
 
 pause
